a9db1f80b9f2aa231719f9c60829542addbdb4c5
DB <ddommor600@g.educaand.es>
2026-02-05 12:26:46 +0100

    Validador incorrecto (Devuelto al predeterminado)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         $rules = [
-            'name'     => 'required|string|max:255',
-            'email'    => 'required|email|unique',
-            'password' => 'required|min:8',
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
 
         $messages = [
